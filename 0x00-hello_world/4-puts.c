5e4a180d726bdfc13387526d82cda689f60a8689
Araka Ebuka <araka.ebuka@gmail.com>
2022-09-08 16:26:52 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio.h>  
-/** 
-* main - Entry point 
-* 
-* Return: Always 0 (Success) 
-*/ 
-int main(void) 
-{ 
-	puts("\"Programming is like building a multilingual puzzle"); 
-	return (0); 
+#include <stdio.h>
+
+/**
+ *  * main - Function starts
+ *   *
+ *    * Return: Function closes
+ *     */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
