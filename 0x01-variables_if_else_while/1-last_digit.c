c62e00ccd03f278b687f6726a89f16e0a1bc2704
Araka Ebuka <araka.ebuka@gmail.com>
2022-09-10 02:03:44 +0100

    Task 3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,11 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- *
- * Return: always return 0
- *
+ * main - lowercase and uppercase
+ * 
+ * Return: Always (Success/Correct)
  */
 int main(void)
 {
