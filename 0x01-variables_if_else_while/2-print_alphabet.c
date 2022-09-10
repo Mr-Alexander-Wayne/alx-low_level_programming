b6bcd693512c427d1cbcee03edaaf92eb6e23ced
Araka Ebuka <araka.ebuka@gmail.com>
2022-09-10 04:10:56 +0100

    I am tired of doing hard things

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints the alphabet in lowercase,
  * followed by a new line
@@ -9,10 +8,10 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch='a'; ch<='z'; ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
-}
+	}
