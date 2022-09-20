105e9e644a128fc56399dea5814729aec89da167
Araka Ebuka <araka.ebuka@gmail.com>
2022-09-20 05:59:43 +0100

    First task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts)half - print the second half of a string
+ * @str: string to use
+ * 
+ */
+
+void outs_half(char *str)
+{
+    int length, n, i;
+
+    for(length = 0; str[length] != '\0'; length++)
+    {
+        
+    }
+}
