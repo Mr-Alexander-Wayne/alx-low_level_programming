e11270ea8ef1888faacdcf5eb88372712c662189
Araka Ebuka <araka.ebuka@gmail.com>
2022-10-06 02:19:21 +0100

    Merge error

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,23 +1,21 @@
 #include "main.h"
+
 /**
  * _strcmp - compares two strings
- * @s1: first string.
- * @s2: second string.
- * Return: 0 if s1 and s2 are equals,
- * another number if not.
+ * @s1: A pointer to change character
+ * @s2: A pointer that will also be changed
+ * Return: string
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
+	char *str_one = s1;
+	char *str_two = s2;
 
-	while (op == 0)
+	while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
 	{
-		if ((*(s1 + i)* == *'\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		str_one++;
+		str_two++;
 	}
-
-	return (op);
+	return (*str_one - *str_two);
 }
